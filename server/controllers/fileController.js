96ce1654ceb487a4aa3ca2bd8ea724d2d24ee4af
SUNITA SATPATHY <sunitasatpathy80@gmail.com>
2026-06-16 16:09:00 +0530

    feat: share link generation with expiry, download limit and password

--- server/controllers/fileController.js
+++ server/controllers/fileController.js
@@ -1,5 +1,7 @@
 const File = require('../models/File');
 const upload = require('../config/multer');
+const { v4: uuidv4 } = require('uuid');
+const bcrypt = require('bcryptjs');
 const path = require('path');
 const fs = require('fs');
 const { encryptFile } = require('../utils/encryption');
@@ -18,7 +20,7 @@ exports.uploadFile = async (req, res) => {
     fs.unlinkSync(uploadedPath);
     const file = await File.create({
       originalName: req.file.originalname,
-      encryptedName: req.file.filename,
+      encryptedName: encryptedFileName,
       size: req.file.size,
       mimeType: req.file.mimetype,
       uploadedBy: req.user.id
@@ -28,5 +30,48 @@ exports.uploadFile = async (req, res) => {
 
   } catch (err) {
     res.status(500).json({ message: 'Upload failed', error: err.message });
+  }
+};
+
+
+exports.generateShareLink = async (req, res) => {
+  try {
+    const { fileId } = req.params;
+    const { expiresIn, downloadLimit, password } = req.body;
+
+    // File dhundo
+    const file = await File.findOne({ _id: fileId, uploadedBy: req.user.id });
+    if (!file) return res.status(404).json({ message: 'File not found' });
+
+    // Expiry set karo
+    const expiryMap = {
+      '1h': 1 * 60 * 60 * 1000,
+      '24h': 24 * 60 * 60 * 1000,
+      '7d': 7 * 24 * 60 * 60 * 1000
+    };
+    const expiresAt = new Date(Date.now() + (expiryMap[expiresIn] || expiryMap['24h']));
+
+    // Password hash karo agar diya ho
+    let hashedPassword = null;
+    if (password) {
+      hashedPassword = await bcrypt.hash(password, 10);
+    }
+
+    // Token generate karo
+    const shareToken = uuidv4();
+
+    // File update karo
+    file.shareToken = shareToken;
+    file.expiresAt = expiresAt;
+    file.downloadLimit = downloadLimit || 1;
+    file.password = hashedPassword;
+    await file.save();
+
+    const shareLink = `http://localhost:5000/api/files/download/${shareToken}`;
+
+    res.json({ shareLink, expiresAt, shareToken });
+
+  } catch (err) {
+    res.status(500).json({ message: 'Error generating link', error: err.message });
   }
 };
--- server/routes/fileRoutes.js
+++ server/routes/fileRoutes.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/authMiddleware');
 const upload = require('../config/multer');
-const { uploadFile } = require('../controllers/fileController');
+const { uploadFile, generateShareLink  } = require('../controllers/fileController');
 
 router.post('/upload', authMiddleware, upload.single('file'), uploadFile);
+router.post('/share/:fileId', authMiddleware, generateShareLink);
 
 module.exports = router;
