90aa6f3aee61fa1ddc5277fa616419737b9f35ce
SUNITA SATPATHY <sunitasatpathy80@gmail.com>
2026-06-18 00:30:24 +0530

    fix: add env variable for API URL and fix CORS

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // Security middleware
 app.use(helmet());
-app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(cors({ origin: '*', credentials: false }));
 app.use(express.json());
 
 // Rate limiting
